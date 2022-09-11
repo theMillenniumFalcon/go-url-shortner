fe1a4d4b01660321a0ffedbb3a393a8b7f8b1bd2
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-11 16:11:05 +0530

    shortener updated

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"os"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/theMillenniumFalcon/url_shortner/database"
+	"github.com/theMillenniumFalcon/url_shortner/helpers"
 )
 
 type request struct {
@@ -27,7 +31,15 @@ func ShortenURL(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
 	}
 
-	// rate implementing
+	// implementing rate limiting
+	redisClient := database.CreateClient(1)
+	defer redisClient.Close()
+	val, err := redisClient.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = redisClient.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ := redisClient.Get(database.Ctx, c.IP()).Result()
+	}
 
 	// check if the input is an actual URL or not
 	if !goValidator.isURl(body.URL) {
